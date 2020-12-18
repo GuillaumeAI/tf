f8b5739a19205dededaf4e246af6a1a9faf0762a
JGWill <jgi@jgwill.com>
2020-12-18 18:19:39 -0500

    fix:tag

--- jgwill-ai-tf1.12-cuda/guillaumeai-build-push.sh
+++ jgwill-ai-tf1.12-cuda/guillaumeai-build-push.sh
@@ -9,8 +9,8 @@ docker push jgwill/ai-tf:1.12.0
 docker build -t guillaumeai/tf:1.12.0 ai-tf1.12
 docker push guillaumeai/tf:1.12.0
 #alias
-docker build -t guillaumeai/tf:1.12.0:gpu ai-tf1.12
-docker push guillaumeai/tf:1.12.0:gpu
+docker build -t guillaumeai/tf:1.12.0-gpu ai-tf1.12
+docker push guillaumeai/tf:1.12.0-gpu
 
 
 docker build -t guillaumeai/tf:1.12.0-cpu ai-tf1.12-cpu
